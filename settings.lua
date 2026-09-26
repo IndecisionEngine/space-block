002ca6d7355447da21252345f9d89136cbc13af9
IndecisionEngine <89117546+IndecisionEngine@users.noreply.github.com>
2026-09-25 23:51:49 -0500

    Actually added the starting item setting

---
+++ settings.lua
@@ -0,0 +1,8 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "space-block-extra-starting-items",
+        setting_type = "startup",
+        default_value = false
+    },
+})
